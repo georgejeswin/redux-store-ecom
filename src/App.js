149d4198d74b9ee074005271a1f74af4fe0b033c
georgejeswin <jeswinmyladoor@gmail.com>
2021-05-02 12:34:55 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
 import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductListing from "./components/ProductListing";
+import ProductDetails from "./components/ProductDetails";
 
 function App() {
   return (
     <div className="app">
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/product/:productId" exact component={ProductDetails} />
+          <Route path="/" exact component={ProductListing} />
+          <Route>
+            <h1>404 Not Found</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/ProductListing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Product from "./Product";
+
+const ProductListing = () => {
+  return (
+    <div className="productListing">
+      <Product />
+    </div>
+  );
+};
+
+export default ProductListing;
