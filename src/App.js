f5dbc15239f8b6a52861f69d18543ea11bb1b710
georgejeswin <jeswinmyladoor@gmail.com>
2021-05-02 12:21:24 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>ShopIt</h1>
+      <Header />
     </div>
   );
 }
